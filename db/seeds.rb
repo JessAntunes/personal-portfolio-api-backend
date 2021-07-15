808fae8f823436b9443749ba6b4e73e37ffb4c60
Jessica Antunes <jessantunes@protonmail.com>
2021-07-14 22:40:37 -0400

    create seed data for projects, start structure for artwork data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,356 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Project.create(
-    type: "Javascript Project",
+Project.create([
+    {type: "Javascript Project",
     mod: "Mod 4",
-    image: "",
+    image: "https://github.com/JessAntunes/logic-grid-frontend/blob/master/images/screenshot.png",
     name: "Logic Grid",
     technology: "Rails Backend | Javascript Frontend",
-    demo: "",
-    code: "",
-    description: ""
-)
+    demo: "https://www.youtube.com/watch?v=OohM3gDU7gM",
+    code: "https://github.com/JessAntunes/logic-grid-frontend",
+    description: "Logic Grid is a website for those who love logic puzzles, but wants a site that has a calm, modern interface that allows for easier concentration without the distractions of ads and pop-ups."},
 
-Project.create(
-    type: "Rails Project",
+    {type: "Rails Project",
     mod: "Mod 3",
-    image: "",
+    image: "https://github.com/JessAntunes/roll-call-for-teachers/blob/master/app/assets/images/screenshot.png",
     name: "Role Call",
     technology: "Ruby on Rails | HTML5 | CSS | OAuth",
-    demo: "",
-    code: "",
-    description: ""
-)
+    demo: "https://www.youtube.com/watch?v=IK4bGTv7UMw",
+    code: "https://github.com/JessAntunes/roll-call-for-teachers",
+    description: "The user for this application is a teacher/professor/lecturer who wishes to create various courses and add students easily to their courses to track courses, students enrolled, and their grades. If a student is not already in the system they can be added. All of the students' grades and courses they are enrolled in are viewable on the student page. By clicking on a student's grade they can be disenrolled from the course or have their grade changed."},
 
-Project.create(
-    type: "Sinatra Project",
+    {type: "Sinatra Project",
     mod: "Mod 2",
-    image: "",
+    image: "https://i.ibb.co/JCDxdv4/Screen-Shot-2021-07-14-at-10-35-08-PM.png",
     name: "Pet Connect",
-    technology: "Rails Backend | Javascript Frontend",
-    demo: "",
-    code: "",
-    description: ""
-)
+    technology: "Sinatra | Bootstrap",
+    demo: "https://www.youtube.com/watch?v=BTiyWoLCl50",
+    code: "https://github.com/JessAntunes/pet-connect-sinatra-project",
+    description: "The user is an organization, typicallly called an animal shelter or SPCA, that houses animals and cares for them until someone is able to adopt them. The organization signs up or logs in to PetConnect and can add animals they currently have in their care and are available for adoption. Along with the typical name, gender, and species attributes, shelters are encouraged to also provide whether an animal is pet or kid friendly. Anyone from the internet can view the shelter's business information and pets that correspond with each facility. Only the shelter needs to login to update their information and add or delete a pet. "},
 
-Project.create(
-    type: "Javascript Project",
-    mod: "Mod 4",
-    image: "",
-    name: "Logic Grid",
-    technology: "Rails Backend | Javascript Frontend",
-    demo: "",
-    code: "",
-    description: ""
-)
+    {type: "Ruby Project",
+    mod: "Mod 1",
+    image: "https://i.ibb.co/yNV7dpF/Screen-Shot-2021-07-14-at-10-25-24-PM.png",
+    name: "Book Search",
+    technology: "Ruby",
+    demo: "https://www.youtube.com/watch?v=dP7B7OLcWGk",
+    code: "https://github.com/JessAntunes/book-search",
+    description: "The user is someone who would like to search by title or topic and get a list of book titles in return. Wants to be able to go back and forth between the results and book information seemlessly to make a decision on what book they want to read. This project not only fullfills the users wants, but also provides a link to the book in Google Books for the rest of the information the API provides about that particular book. This will allow the user to gain more information to obtain the book, such as how to view it for free."}
+])
+
+
+Artwork.create([
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+    {photo: "",
+    type: ,
+    animal: ,
+    custom: },
+
+])
+
+
+
+
+
+puts "Seeded and Succeeded"
+
